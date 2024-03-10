6d2b119ff87c032c9349b54fc50345e086255f16
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-03-10 19:57:21 +0530

    UI/UX updates

--- app/src/main/java/com/example/extravaganza/MainActivity.java
+++ app/src/main/java/com/example/extravaganza/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.extravaganza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
       // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,travelled_location.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/extravaganza/travelled_location.java
@@ -0,0 +1,14 @@
+package com.example.extravaganza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class travelled_location extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travelled_location);
+    }
+}
