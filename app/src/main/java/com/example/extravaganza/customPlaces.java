8e5dafbc629b60beac2007323953fd03df1f9627
Sahil Agrawal <agrawalsahil4994@gmail.com>
2024-03-12 01:28:27 +0530

    UI/UX updates

---
+++ app/src/main/java/com/example/extravaganza/customPlaces.java
@@ -0,0 +1,28 @@
+package com.example.extravaganza;
+
+public class customPlaces {
+    private String place;
+    private String date;
+    private int ImageId;
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public customPlaces(String place, String date){
+        this.place=place;
+        this.date=date;
+    }
+}
